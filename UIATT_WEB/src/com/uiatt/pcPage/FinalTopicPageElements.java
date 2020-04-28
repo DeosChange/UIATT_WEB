6cd885f0d08b2ec495d5894454bb666f90b8a70a
change <jiangshiyuhua@sina.cn>
2020-04-28 20:26:52 +0800

    Update FinalTopicPageElements.java

--- UIATT_WEB/src/com/uiatt/pcPage/FinalTopicPageElements.java
+++ UIATT_WEB/src/com/uiatt/pcPage/FinalTopicPageElements.java
@@ -9,7 +9,7 @@ import org.testng.annotations.Listeners;
  * */
 
 @Listeners({com.uiatt.listener.AssertionListener.class})
-public class FinalTopicPageElements {
+public class FinalTopicPageElements extends BaseElements{
 	//正在热议的第一篇话题
 	public static final String FIRST_TOPIC = ".//ul[@class='v_l_list']/li[1]/div[2]/div[1]/a";
 
